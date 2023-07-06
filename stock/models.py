0f3468e282b98d9a05252e98716d8ff652da68f5
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-07-06 07:19:12 +0300

    add apps

--- stock/models.py
+++ stock/models.py
@@ -10,4 +10,4 @@ class Stock(models.Model):
     date_created=models.DateTimeField(auto_now_add=True)
     date_updated=models.DateTimeField(auto_now=True)
     def __str__(self):
-        return self.name
+        return self.name   
