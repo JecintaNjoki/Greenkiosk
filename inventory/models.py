a52479f72e4ffff19d74ad4719330c5042a52812
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-07-19 17:27:56 +0300

    add

--- inventory/models.py
+++ inventory/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from Vendor.models import Vendor
+from Customer.models import Customer
 
 # Create your models here.
 class Products(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    Vendor=models.ForeignKey(Vendor,null =True, on_delete =models.CASCADE)
     name=models.CharField(max_length=32)
     price=models.DecimalField(max_digits=8,decimal_places=2)
     stock=models.PositiveBigIntegerField()
@@ -9,6 +13,7 @@ class Products(models.Model):
     description=models.TextField()
     date_created=models.DateTimeField(auto_now_add=True)
     date_updated=models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
     
