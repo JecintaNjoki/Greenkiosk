4bd7598cd1949994e3e50f00c07a48a5360f832a
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-07-20 09:47:07 +0300

    add

--- vendor/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Vendor
-class VendorAdmin(admin.ModelAdmin):
-    list_display=("first_name","last_name","email","password","date_created","date_updated")
-
-admin.site.register(Vendor,VendorAdmin)
